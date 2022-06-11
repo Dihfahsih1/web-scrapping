5602ecaa9a33474790273d018aa937dce4ea7f4f
Dihfahsih1 <dihfahsihm@gmail.com>
2022-06-11 23:55:22 +0300

    scrap blogs and news hits for www.owl.health website

--- web-scrap.py
+++ web-scrap.py
@@ -4,39 +4,34 @@ from bs4 import BeautifulSoup
 import json
 output=[]
 
-#news data
-news_url="https://www.owl.health/category/media-hits/"
-news_page = requests.get(news_url).text
-news_doc=BeautifulSoup(news_page, 'html.parser')
-news_pages = int(news_doc.find(class_='alignleft').a['href'].split('/')[-2])
+# #news data
+# news_url="https://www.owl.health/category/media-hits/"
+# news_page = requests.get(news_url).text
+# news_doc=BeautifulSoup(news_page, 'html.parser')
+# news_pages = int(news_doc.find(class_='alignleft').a['href'].split('/')[-2])
 
-for page in range(1, news_pages + 1 ):
-  new_url=f"https://www.owl.health/category/media-hits/page/{page}/?et_blog"
-  news_page = requests.get(news_url).text
-  news_doc=BeautifulSoup(news_page, 'html.parser')
-  items = news_doc.find_all(class_="et_pb_post")
-  for item in items:
-    title =item.find(class_="entry-title")
-    get_link = item.find('a')
-    link=get_link.attrs['href']
-    get_details = requests.get(link).text 
-    detail_page = BeautifulSoup(get_details, 'html.parser')
-    details=detail_page.find(class_="field-name-body")
-    try:
-      print(details.text)
-    except:
-      pass
+# for page in range(1, news_pages + 1 ):
+#   new_url=f"https://www.owl.health/category/media-hits/page/{page}/?et_blog"
+#   news_page = requests.get(news_url).text
+#   news_doc=BeautifulSoup(news_page, 'html.parser')
+#   items = news_doc.find_all(class_="et_pb_post")
+#   for item in items:
+#     title =item.find(class_="entry-title")
+#     get_link = item.find('a')
+#     link=get_link.attrs['href']
+#     get_details = requests.get(link).text 
+#     detail_page = BeautifulSoup(get_details, 'html.parser')
+#     details=detail_page.find(class_="field-name-body")
+#     news={}
+#     news["News Title: "]=title.text
+#     try:
+#       news["News Description: "]=details.text
+#     except:
+#       pass
     
-    news={}
-    news["News Title: "]=title.text
-    try:
-      news["News Description: "]=details.text
-    except:
-      pass
-    
-    output.append(news)
+#     output.append(news)
 
-blog data   
+#blog data   
 blog_url="https://www.owl.health/blog/"
 blog_page = requests.get(blog_url).text
 blog_doc=BeautifulSoup(blog_page, 'html.parser')
@@ -64,7 +59,7 @@ for page in range(1, blog_pages + 1 ):
       pass
     
     output.append(blog)
-with open("outfile.json", "w") as f:
+with open("blogs-data.json", "w") as f:
   f.write(json.dumps(output))
   
 
