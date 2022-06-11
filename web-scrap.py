00ea38d12693203f8454acd0ad998e32a031a812
Dihfahsih1 <dihfahsihm@gmail.com>
2022-06-11 11:02:10 +0300

    get pages to be scrapped

--- web-scrap.py
+++ web-scrap.py
@@ -1,13 +1,18 @@
 import requests
 
 from bs4 import BeautifulSoup
+url="https://www.owl.health/blog/"
+page = requests.get(url).text
+doc=BeautifulSoup(page, 'html.parser')
+page_text = doc.find(class_='alignleft').a['href'].split('/')[-2]
 
-result = requests.get("https://www.owl.health/category/case-studies/")
-src=result.content
-soup=BeautifulSoup(src, 'lxml')
-urls = []
+print(page_text)
 
-for h2_tag in soup.find_all("h2"):
-  a_tag = h2_tag.find("a")
-  urls.append(a_tag.attrs['href'])
-print(urls)
+# src=result.content
+# soup=BeautifulSoup(src, 'lxml')
+# urls = []
+
+# for h2_tag in soup.find_all("h2"):
+#   a_tag = h2_tag.find("a")
+#   urls.append(a_tag.attrs['href'])
+# print(urls)
