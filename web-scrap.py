328a5c4e4fdc4ddcd9df6d9ac54adcbb0ebb55d5
Dihfahsih1 <dihfahsihm@gmail.com>
2022-06-11 18:54:06 +0300

    get post details

--- web-scrap.py
+++ web-scrap.py
@@ -1,12 +1,29 @@
 import requests
-
 from bs4 import BeautifulSoup
 url="https://www.owl.health/blog/"
 page = requests.get(url).text
 doc=BeautifulSoup(page, 'html.parser')
-page_text = doc.find(class_='alignleft').a['href'].split('/')[-2]
+pages = int(doc.find(class_='alignleft').a['href'].split('/')[-2])
 
-print(page_text)
+for page in range(1, pages + 1 ):
+  url=f"https://www.owl.health/blog/page/{page}/?et_blog"
+  page = requests.get(url).text
+  doc=BeautifulSoup(page, 'html.parser')
+  items = doc.find_all(class_="et_pb_post")
+  for item in items:
+    title =item.find(class_="entry-title")
+    get_link = item.find('a')
+    link=get_link.attrs['href']
+    get_details = requests.get(link).text 
+    detail_page = BeautifulSoup(get_details, 'html.parser')
+    details=detail_page.find(class_="et_pb_post_content")
+    description=item.find(class_="post-content-inner")
+     
+    print("Blog Title: " + title.text)
+    print("Blog Description: " + details.text)
+    print("link : " + link)
+    print("-------------------------")
+  
 
 # src=result.content
 # soup=BeautifulSoup(src, 'lxml')
